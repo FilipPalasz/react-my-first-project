3455f48e1fb97b8f589856ab7fcc06d188f9e972
FilipPalasz <eaa23fap@students.eaaa.dk>
2024-08-27 10:52:30 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import User from "./components/User"
+
+function App() {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers(){
+    const response = await fetch(
+      "https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"
+    );
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  return(
+    <main className="app">
+      <h1>My First React App!!</h1>
+
+        <section className="grid">
+          {users.map(user=> (
+            <User key={user.id} name={user.name} mail={user.mail} image={user.image} />
+        ))}
+        </section>
+    </main>
+  );
+}
+
+export default App
